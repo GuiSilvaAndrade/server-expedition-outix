bed1406cab6fd134ab2b6823a95f4709956d3561
Guilherme <guisilvandrade@gmail.com>
2025-08-26 20:23:37 -0300

    Dynamic implementation of JSON product

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 from services.nf_data import *
+
 
 app = Flask(__name__)
 CORS(app)
@@ -9,13 +9,11 @@ CORS(app)
 @app.route("/", methods=['POST'])
 def home():
     barcode = request.get_json().get('value')
-    print(barcode)
 
-    costumer, num_NF, product, qty = nf_data(barcode)
+    response = nf_data(barcode)
+    
+    return response
 
-    print(costumer, num_NF, product, qty)
-
-    return jsonify({"resultado": f'{costumer} {num_NF} {product} {qty}'})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- services/nf_data.py
+++ services/nf_data.py
@@ -1,18 +1,33 @@
 from xml.dom import minidom
+from flask import jsonify
 
 def nf_data(bar_code):
 
     xml = minidom.parse(open(f'files/{bar_code}-nfe.xml'))
-    
-    num_NF = xml.getElementsByTagName('ide')[0].getElementsByTagName('nNF')[0].firstChild.data
-    customer = xml.getElementsByTagName('dest')[0].getElementsByTagName('xNome')[0].firstChild.data 
+    products = []
 
-    # NFe itens
+    # NFe products
     itens = xml.getElementsByTagName('det')
-    sku, qty, product = [], [], []
-    for item in itens:
-        sku.append(item.getElementsByTagName('cProd')[0].firstChild.data)
-        qty.append(item.getElementsByTagName('qCom')[0].firstChild.data)
-        product.append(item.getElementsByTagName('xProd')[0].firstChild.data)
 
-    return customer, num_NF, product, qty
+    for item in itens:
+        sku = item.getElementsByTagName('cProd')[0].firstChild.data
+        qty = item.getElementsByTagName('qCom')[0].firstChild.data
+        product_name = item.getElementsByTagName('xProd')[0].firstChild.data
+
+        product = {
+            'sku': sku,
+            'qty': qty,
+            'product_name': product_name            
+        }
+        products.append(product)     
+
+    num_NF = xml.getElementsByTagName('ide')[0].getElementsByTagName('nNF')[0].firstChild.data
+    customer = xml.getElementsByTagName('dest')[0].getElementsByTagName('xNome')[0].firstChild.data
+
+    response = jsonify({
+        'customer': customer,
+        'num_nf': num_NF,        
+        'products': products
+        })       
+
+    return response
